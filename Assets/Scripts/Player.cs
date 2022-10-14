636f4b896d2a0ccc3803220249ca3595e37fe96e
kolbasist <101858030+kolbasist@users.noreply.github.com>
2022-10-14 22:00:56 +0300

    preprealfa

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+    [SerializeField] private List<Weapon> _weapons;
+    [SerializeField] private Transform _shootpoint;
+
+    private Weapon _currentWeapon;
+    private int _currentHealth;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _currentWeapon = _weapons[0];
+        _currentHealth = _health;
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _currentWeapon.Shoot(_shootpoint);
+        }
+    }
+}
